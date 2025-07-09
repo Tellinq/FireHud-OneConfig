aa3d37c6b4d97324870d78269367a6aab0cffeb8
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-07-08 23:17:06 -0400

    Add Fire Tint for 1.20+

--- src/main/java/dev/tellinq/firehud/client/FireHud.java
+++ src/main/java/dev/tellinq/firehud/client/FireHud.java
@@ -1,14 +1,7 @@
 package dev.tellinq.firehud.client;
 
-//import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
-//import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import dev.deftu.omnicore.common.OmniIdentifier;
 import dev.tellinq.firehud.FireHudConstants;
-import dev.tellinq.firehud.client.accessor.Accessor_SoulFireEntity;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import dev.tellinq.firehud.client.command.FireHudCommand;
 import dev.tellinq.firehud.client.config.FireHudConfig;
@@ -21,27 +14,6 @@ public class FireHud {
         FireHudConfig.INSTANCE.preload();
         CommandManager.register(new FireHudCommand());
     }
-
-    // TODO: Make Fire Tint work and backport it to 1.19 and older as well
-    //#if MC >= 1.20
-    private void fireTint(DrawContext context) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        PlayerEntity player = client.player;
-        int width = context.getScaledWindowWidth();
-        int height = context.getScaledWindowHeight();
-
-        if (player != null && player.isOnFire() && client.options.getPerspective().isFirstPerson() &&
-                !(!FireHudConfig.FirstPersonFire.whenInLava && player.isInLava()) && !(!FireHudConfig.FirstPersonFire.fireResistance && player.hasStatusEffect(StatusEffects.FIRE_RESISTANCE))) {
-
-            if (FireHudConfig.FireScreenTint.enabled && !((Accessor_SoulFireEntity) player).fireHud$isOnSoulFire()) {
-                context.fillGradient(0, 0, width, height, FireHudConfig.FireScreenTint.tintStartColor.getArgb(), FireHudConfig.FireScreenTint.tintEndColor.getArgb());
-            }
-            if (FireHudConfig.FireScreenTint.enabled && FireHudConfig.renderSoulFire && ((Accessor_SoulFireEntity) player).fireHud$isOnSoulFire()) {
-                context.fillGradient(0, 0, width, height, FireHudConfig.FireScreenTint.soulTintStartColor.getArgb(), FireHudConfig.FireScreenTint.soulTintEndColor.getArgb());
-            }
-        }
-    }
-    //#endif
 
     public static Identifier getFireHudResource(String path) {
         return OmniIdentifier.create(FireHudConstants.ID, path);
---
+++ src/main/java/dev/tellinq/firehud/client/mixin/feature/firetint/Mixin_InGameHud_FireTint.java
@@ -0,0 +1,75 @@
+package dev.tellinq.firehud.client.mixin.feature.firetint;
+
+import dev.deftu.omnicore.client.OmniClient;
+import dev.deftu.omnicore.client.render.OmniResolution;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import dev.tellinq.firehud.client.accessor.Accessor_SoulFireEntity;
+import dev.tellinq.firehud.client.config.FireHudConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+//#if MC <= 1.21.1
+//$$ import com.mojang.blaze3d.systems.RenderSystem;
+//#endif
+
+//#if MC >= 1.21
+import net.minecraft.client.render.RenderTickCounter;
+//#endif
+
+//#if MC <= 1.19.4
+//$$ import net.minecraft.client.util.math.MatrixStack;
+//#endif
+
+@Mixin(InGameHud.class)
+public class Mixin_InGameHud_FireTint {
+    @Inject(
+            //#if MC >= 1.20.5
+            method = "renderMiscOverlays",
+            //#else
+            //$$ method = "render",
+            //#endif
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getFrozenTicks()I"))
+    private void fireHud$render(
+            //#if MC <= 1.19.4
+            //$$ MatrixStack matrices,
+            //#else
+            DrawContext context,
+            //#endif
+            //#if MC >= 1.21
+            RenderTickCounter tickCounter,
+            //#else
+            //$$ float tickDelta,
+            //#endif
+            CallbackInfo ci) {
+        MinecraftClient client = OmniClient.getInstance();
+        PlayerEntity player = client.player;
+
+        if (player == null) return;
+
+        int width = OmniResolution.getScaledWidth();
+        int height = OmniResolution.getScaledHeight();
+
+        boolean isFirstPerson = client.options.getPerspective().isFirstPerson();
+        boolean isOnFire = player.isOnFire();
+        boolean isInLava = player.isInLava();
+        boolean hasFireResistance = player.hasStatusEffect(StatusEffects.FIRE_RESISTANCE);
+        boolean showInLava = FireHudConfig.FirstPersonFire.whenInLava || !isInLava;
+        boolean showWithFireRes = FireHudConfig.FirstPersonFire.fireResistance || !hasFireResistance;
+
+        if (FireHudConfig.FireScreenTint.enabled && isOnFire && isFirstPerson && showInLava && showWithFireRes) {
+                boolean isOnSoulFire = ((Accessor_SoulFireEntity) player).fireHud$isOnSoulFire() && FireHudConfig.renderSoulFire;
+
+                //#if MC <= 1.19.4
+                context.fillGradient(0, 0, width, height,
+                        isOnSoulFire ? FireHudConfig.FireScreenTint.soulTintStartColor.getArgb() : FireHudConfig.FireScreenTint.tintStartColor.getArgb(),
+                        isOnSoulFire ? FireHudConfig.FireScreenTint.soulTintEndColor.getArgb() : FireHudConfig.FireScreenTint.tintEndColor.getArgb());
+                //#endif
+        }
+    }
+}
