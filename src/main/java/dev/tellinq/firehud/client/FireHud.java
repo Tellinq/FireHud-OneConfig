b042608bd0a93079747b1e748bf7023aca60dc71
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-07-09 22:36:24 -0400

    Add Fire Resistance Timer Now these should be 100% feature parity aside from some small HUD discrepencies such as coloring, but that can be addressed later on

--- src/main/java/dev/tellinq/firehud/client/FireHud.java
+++ src/main/java/dev/tellinq/firehud/client/FireHud.java
@@ -2,10 +2,12 @@ package dev.tellinq.firehud.client;
 
 import dev.deftu.omnicore.common.OmniIdentifier;
 import dev.tellinq.firehud.FireHudConstants;
+import dev.tellinq.firehud.client.hud.FireResistanceTimer;
 import net.minecraft.util.Identifier;
 import dev.tellinq.firehud.client.command.FireHudCommand;
 import dev.tellinq.firehud.client.config.FireHudConfig;
 import org.polyfrost.oneconfig.api.commands.v1.CommandManager;
+import org.polyfrost.oneconfig.api.hud.v1.HudManager;
 
 public class FireHud {
     public static final FireHud INSTANCE = new FireHud();
@@ -13,6 +15,7 @@ public class FireHud {
     public void initialize() {
         FireHudConfig.INSTANCE.preload();
         CommandManager.register(new FireHudCommand());
+        HudManager.register(new FireResistanceTimer());
     }
 
     public static Identifier getFireHudResource(String path) {
---
+++ src/main/java/dev/tellinq/firehud/client/hud/FireResistanceTimer.java
@@ -0,0 +1,47 @@
+package dev.tellinq.firehud.client.hud;
+
+import dev.deftu.omnicore.client.OmniClient;
+import dev.deftu.omnicore.client.OmniClientPlayer;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffectUtil;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
+import org.polyfrost.oneconfig.api.config.v1.annotations.Switch;
+import org.polyfrost.oneconfig.api.hud.v1.TextHud;
+
+import java.util.Objects;
+
+public class FireResistanceTimer extends TextHud {
+
+    @Switch(
+            title = "Show as Ticks"
+    )
+    public boolean showAsTicks = false;
+
+    public FireResistanceTimer() {
+        super("fireresistancetimer", "Fire Resistance Timer", Category.getINFO(), "Fire Resistance:", "");
+    }
+
+    @Override
+    protected @Nullable String getText() {
+        ClientWorld world = OmniClient.getInstance().world;
+        ClientPlayerEntity player = OmniClientPlayer.getInstance();
+
+        if (player != null && world != null) {
+            StatusEffectInstance fireRes = player.getStatusEffect(StatusEffects.FIRE_RESISTANCE);
+
+            if (player.hasStatusEffect(StatusEffects.FIRE_RESISTANCE) && fireRes != null && !fireRes.isInfinite()) {
+
+                Text durationSeconds = StatusEffectUtil.getDurationText(fireRes, 1.0f, world.getTickManager().getTickRate());
+                Text durationTicks = Text.literal(String.valueOf(Objects.requireNonNull(player.getStatusEffect(StatusEffects.FIRE_RESISTANCE)).getDuration()));
+                Text text = Text.literal((this.showAsTicks ? durationTicks : durationSeconds).getString());
+                return text.getString();
+            }
+        }
+
+        return "00:00";
+    }
+}
