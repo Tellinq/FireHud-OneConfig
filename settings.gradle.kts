243fc531738f983dc1ad4fbf59b46e163cca8291
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-09-18 21:51:53 -0400

    Update kotlin jvm

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("jvm") version("2.0.20")
+        kotlin("jvm") version("2.2.10")
         id("dev.deftu.gradle.multiversion-root") version("2.+")
     }
 }
