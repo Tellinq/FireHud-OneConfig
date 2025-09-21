fe1f8fa0190eadbc11fd3ba85300ae7bc6be4442
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-09-21 12:42:23 -0400

    Lock DGT to 2.53.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ pluginManagement {
 
     plugins {
         kotlin("jvm") version("2.2.10")
-        id("dev.deftu.gradle.multiversion-root") version("2.+")
+        id("dev.deftu.gradle.multiversion-root") version("2.53.0")
     }
 }
 
